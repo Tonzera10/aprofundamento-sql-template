0a6f51792b315c03c4ad7720d58b39f1c0267618
Everton Mello <everton.mello.281@gmail.com>
2023-03-29 21:45:06 -0300

    aula ok

--- database/pratica-aprofundamento-sql.sql
+++ database/pratica-aprofundamento-sql.sql
@@ -1,3 +1,4 @@
+-- Active: 1680128734479@@127.0.0.1@3306
 -- Conecte o arquivo pratica-aprofundamento-sql.db com a extensão MySQL e ative a conexão aqui
 
 -- Deletar tabela
@@ -42,4 +43,43 @@ VALUES
 -- Buscar todos os pokemons
 SELECT * FROM pokemons;
 
--- Práticas
+-- Prática guiada 1
+SELECT * FROM pokemons WHERE speed > 60; --todos que speed for maior que 60 
+
+SELECT * FROM pokemons WHERE attack > 60 AND special_attack > 60; --todos q for attack e special_attack maior que 60
+
+SELECT * FROM pokemons WHERE name LIKE "%saur"; -- todos que terminarem em saur
+
+-- Prática guiada 2
+
+SELECT * FROM pokemons;
+
+SELECT AVG(hp) FROM pokemons; -- media do hp
+
+--ROUND() arredondar valores
+
+SELECT COUNT(*) FROM pokemons; --conta a quantidade de linhas
+
+SELECT AVG(hp) AS mediaSimplesHp FROM pokemons;
+
+
+SELECT COUNT(*) AS numeroLinhasDaTabela FROM pokemons;
+
+SELECT SUM(hp) AS somaDaColunaHp FROM pokemons;
+
+-- Prática guiada 3
+
+SELECT * FROM pokemons ORDER BY defense ASC; -- mostra ordenando por defence crescente
+
+SELECT * FROM pokemons ORDER BY defense DESC; -- mostra ordenando por defence decrescente
+
+SELECT COUNT(*) AS qtdPokemons, type FROM pokemons ORDER BY type;
+
+SELECT * FROM pokemons LIMIT 3 OFFSET 4; -- mostra um limite de 3 itens, a partir do item 4
+
+--Para gravar
+
+SELECT * FROM pokemons 
+WHERE type = "fire" OR type = "grass" 
+ORDER BY attack ASC 
+LIMIT 3 OFFSET 3;
